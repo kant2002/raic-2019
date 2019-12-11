3957aeacbced3970077cca06a02075728d610520
Andrii Kurdiumov <kant2002@gmail.com>
2019-12-11 19:43:49 +0200

    Improve debugging interface to operate on higher level things

--- clients/cpp/Debug.cpp
+++ clients/cpp/Debug.cpp
@@ -1,11 +1,54 @@
 #include "Debug.hpp"
 #include "model/PlayerMessageGame.hpp"
 
-Debug::Debug(const std::shared_ptr<OutputStream> &outputStream)
+Debug::Debug(const std::shared_ptr<OutputStream>& outputStream)
     : outputStream(outputStream) {}
 
-void Debug::draw(const CustomData &customData) {
-  outputStream->write(PlayerMessageGame::CustomDataMessage::TAG);
-  customData.writeTo(*outputStream);
-  outputStream->flush();
+void Debug::draw(const CustomData& customData) {
+    outputStream->write(PlayerMessageGame::CustomDataMessage::TAG);
+    customData.writeTo(*outputStream);
+    outputStream->flush();
+}
+
+void Debug::mark(const Unit& unit) {
+    this->draw(
+        CustomData::Rect(
+            Vec2Float(unit.position.x, unit.position.y),
+            Vec2Float(unit.size.x, unit.size.y),
+            ColorFloat(1, 0, 0, 0.5)));
+}
+void Debug::mark(const Vec2Double& position) {
+    this->draw(
+        CustomData::Rect(
+            Vec2Float(position.x - 0.05, position.y - 0.05),
+            Vec2Float(0.1, 0.1),
+            ColorFloat(1, 0, 0, 0.5)));
+}
+
+void Debug::mark(const LootBox& unit) {
+    this->draw(
+        CustomData::Rect(
+            Vec2Float(unit.position.x, unit.position.y),
+            Vec2Float(unit.size.x, unit.size.y),
+            ColorFloat(0, 1, 0, 0.5)));
+}
+
+void Debug::drawAim(const Vec2Double& aim, const Unit& unit)
+{
+    if (unit.weapon == nullptr) {
+        return;
+    }
+
+    if (unit.weapon->lastAngle == nullptr) {
+        return;
+    }
+
+    auto dx = unit.position.x - aim.x;
+    auto dy = unit.position.y - aim.y;
+    this->draw(
+        CustomData::Line(
+            Vec2Float(unit.position.x, unit.position.y),
+            Vec2Float(aim.x, aim.y),
+            0.1,
+            ColorFloat(0, 1, 0, 0.5)));
 }
--- clients/cpp/Debug.hpp
+++ clients/cpp/Debug.hpp
@@ -3,15 +3,20 @@
 
 #include "Stream.hpp"
 #include "model/CustomData.hpp"
+#include "model/Unit.hpp"
+#include "model/LootBox.hpp"
 #include <memory>
 
 class Debug {
 public:
-  Debug(const std::shared_ptr<OutputStream> &outputStream);
-  void draw(const CustomData &customData);
-
+    Debug(const std::shared_ptr<OutputStream>& outputStream);
+    void draw(const CustomData& customData);
+    void mark(const Unit& unit);
+    void mark(const Vec2Double& position);
+    void mark(const LootBox& lootbox);
+    void drawAim(const Vec2Double& aim, const Unit& unit);
 private:
-  std::shared_ptr<OutputStream> outputStream;
+    std::shared_ptr<OutputStream> outputStream;
 };
 
 #endif
